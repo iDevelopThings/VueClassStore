0d9f02c49fd53cc45752f545ff4056af940c05d0
Sam Parton <sam@idevelopthings.com>
2021-08-05 20:36:03 +0100

    Built assets ready for publishing

--- package/index.js
+++ package/index.js
@@ -11,5 +11,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./Store"), exports);
-__exportStar(require("./WebpackPlugin"), exports);
+__exportStar(require("./VueClassStoresPlugin"), exports);
 //# sourceMappingURL=index.js.map
