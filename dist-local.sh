6772db4674ab366cc27c67d2888f8a7e9ec42de2
Sam Parton <sam@idevelopthings.com>
2021-08-12 18:42:11 +0100

    Update dist-local.sh

--- dist-local.sh
+++ dist-local.sh
@@ -7,14 +7,5 @@ cp README.md dist/README.md
 
 cp package.json dist/package.json
 
-cd dist || exit
 
-npm link
 
-cd /Users/sam/Code/RustGamblingSite/client || exit
-
-npm link vue-class-stores
-
-cd /Users/sam/Code/Packages/vue-cli-plugin-class-stores || exit
-
-npm link vue-class-stores
